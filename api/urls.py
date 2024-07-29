edf1a759847c293db2c461163ab0b7b2d45a7a84
aswanthabam <aswanth.abam@gmail.com>
2024-07-29 16:44:20 +0530

    FEAT : Minor improvements

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 
     path('posts/', GetPostsView.as_view()),
     path('posts/create/', PostView.as_view()),
-    path('posts/<str:slug>/', PostView.as_view()),
+    path('posts/<str:slug>/', GetPostsView.as_view()),
     path('posts/<str:slug>/edit/', PostView.as_view()),
     path('posts/<str:slug>/delete/', PostView.as_view())
 ]
